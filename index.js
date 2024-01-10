2829a13d193e43fff1c42ffaa5600798890cbbe5
Gaurav Lokhande <lokhandeg570@gmial.com>
2024-01-10 12:21:43 +0530

    Changed origin

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
